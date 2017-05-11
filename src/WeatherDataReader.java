1748929c12b743e1b396241287b1d68dafcc1da4
Jacob Moxham <jacobmoxham@live.co.uk>
2017-05-11 17:20:45 +0100

    Added Dependencies
    Converted this to a Maven project, added dependencies for openweathermap
    and JavaFX. If this causes errors for you guys let me know and I'll help
    you fix it

--- src/WeatherDataReader.java
+++ src/WeatherDataReader.java
@@ -1,6 +1,10 @@
+import org.bitpipeline.lib.owm.OwmClient;
 
 public class WeatherDataReader {
 	public static weatherObject getDataForNow(){
+		
+		OwmClient owm = new OwmClient ();
+		
 		return null;
 	}
 }
