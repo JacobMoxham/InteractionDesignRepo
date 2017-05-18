b1e859d97339220e5787db69111543b2a94f84f2
Jacob Moxham <jacobmoxham@live.co.uk>
2017-05-18 20:23:48 +0100

    Correct colours, transparent background, transparent button over oars only clickable if it should be

--- src/BladeController.java
+++ src/BladeController.java
@@ -1,12 +1,17 @@
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.util.converter.TimeStringConverter;
 
 public class BladeController {
+	
+	private boolean clickable;
+	
 	@FXML
 	private ImageView windImageObj;
 	@FXML
@@ -19,6 +24,10 @@ public class BladeController {
 	private Text time;
 	@FXML
 	private Text day;
+	@FXML
+	private Button bladeButton;
+	@FXML
+	private ImageView bladeImage;
 	
 	@FXML
 	private StringProperty windDegreeString = new SimpleStringProperty();
@@ -32,39 +41,59 @@ public class BladeController {
 	private StringProperty timeString= new SimpleStringProperty();
 	@FXML
 	private StringProperty dayString= new SimpleStringProperty();
+	@FXML
+	private StringProperty bladeString = new SimpleStringProperty();
 	
 	//Reference to application
 	private MainApp mainApp;
 	
 	public void initialize(){
+		//Set the components based on the string objects
 		this.time.setText(timeString.get());
 		this.windTextObj.setText(windText.get().split("\\.")[0]);
 		this.weather.setImage(new Image(weatherImage.get()));
 		this.day.setText(dayString.get().split("\\s")[0]);
-		this.degrees.setText(degreesString.get());
+		this.degrees.setText(degreesString.get()+"\u00b0"+"C");
 		this.windImageObj.setImage(new Image("winddirection.png"));
+		//Rotates the wind image based on the wind degree
 		this.windImageObj.setRotate(Double.parseDouble(windDegreeString.get()));
-		
-		// aidens addition - windDegrees and setWindImage
-		//this.windImageObj = 
-		
+		this.bladeImage.setImage(new Image(bladeString.get()));
 	}
 	
-	public BladeController(String temp,String time,String iconURL,String windDegree,String windSpeed,String date,Boolean isClickable){
-		
-	}
-	public BladeController(String temp,String time,String iconURL,String windDegree,String windSpeed,String date){
+	public BladeController(String temp,String time,String iconURL,String windDegree,String windSpeed,String date,Boolean clickable){
+		//Set string variables
 		timeString.set(time);
 		weatherImage.set(iconURL);
 		degreesString.set(temp);
 		dayString.set(date);
 		windText.set(windSpeed);
-		
-		// aidens addition - rotate according to degrees
 		windDegreeString.set(windDegree);
+		this.clickable = clickable;
+		if (clickable){
+			bladeString.set("500px-Rowing_Tan.png");
+		}else{
+			bladeString.set("500px-Rowing_Blade_Cambridge Blue.png");
+		}
+	}
+	public BladeController(String temp,String time,String iconURL,String windDegree,String windSpeed,String date){
+		this(temp,time,iconURL,windDegree,windSpeed,date,false);
+	}
+	@FXML
+	public void bladePress(){
+		if(clickable){
+			System.out.println("click");
+		}
 	}
 	
 	public void setMainApp(MainApp mainApp){
 		this.mainApp = mainApp;
+	}
+
+	public boolean isClickable() {
+		return clickable;
+	}
+
+	public void setClickable(boolean clickable) {
+		this.clickable = clickable;
 	}
 }
--- src/By3HoursController.java
+++ src/By3HoursController.java
@@ -5,10 +5,12 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
 public class By3HoursController {
 	@FXML
@@ -32,31 +34,39 @@ public class By3HoursController {
 		try {
 			//Populate VBOX
 			ObservableList<Node> forecasts = FXCollections.observableArrayList();
-			int total = 0;
+			boolean notFirst = false;
+			
+			//test value
+			//int total = 0;
+			
 			for(List<WeatherObject> l : WeatherDataReader.getNextFiveDaysHourly()){
+				//Adds the correct week Day label
+				if (notFirst){
+					Label label = new Label(l.get(0).getDate().split("\\s")[0]);
+					label.setAlignment(Pos.CENTER);
+					label.setFont(new Font("Arial", 24.0));
+					forecasts.add(label);
+				}
+				//Ensures not weekday label for today
+				notFirst = true;
 				for(WeatherObject w: l){
 					FXMLLoader loader = new FXMLLoader();
 					loader.setController(new BladeController(w.getTemp(),w.getTime(),w.getIconURL(),w.getWindDegree(),w.getWindSpeed(),w.getDate()));
 					loader.setLocation(MainApp.class.getResource("Blade.fxml"));
 					Node thisBlade = (Node) loader.load();
 					forecasts.add(thisBlade);
-					total++;
+					//total++;
 				}
-				//TODO: Add label
-				Label label = new Label("This is a day label");
-				forecasts.add(label);
 			}
 			
 			//test
-			System.out.println("No Blades:" + total);
-			
-			
+			//System.out.println("No Blades:" + total);
 			
 			weatherForecasts.getChildren().addAll(forecasts);
 			
 			//test
-			System.out.println("No children of VBox: "+weatherForecasts.getChildren().size());
-			System.out.println();
+			//System.out.println("No children of VBox: "+weatherForecasts.getChildren().size());
+			//System.out.println();
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
