3486b9ba8e22b62127870d0f7aec6872444769f2
Jacob Moxham <jacobmoxham@live.co.uk>
2017-05-21 21:36:18 +0100

    Huge performance increase, all delays moved to loading up

--- src/By3HoursController.java
+++ src/By3HoursController.java
@@ -1,26 +1,6 @@
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.List;
-import java.util.Date;
-
-import javax.print.attribute.standard.DateTimeAtCompleted;
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseDragEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 
 public class By3HoursController {
 	
@@ -39,103 +19,8 @@ public class By3HoursController {
 	
 	public void setMainApp(SplashScreenApp splashScreenApp){
 		this.mainApp = splashScreenApp;	
-		try {
-		
-			//Populate VBOX
-			ObservableList<Node> forecasts = FXCollections.observableArrayList();
-			FXMLLoader bladeLoader = new FXMLLoader(MainApp.class.getResource("Blade.fxml"));
-			FXMLLoader miniflagLoader = new FXMLLoader(MainApp.class.getResource("MiniFlag.fxml"));
-			
-			//Add miniflag to top
-			try{
-				Node miniflag = (Node) miniflagLoader.load();
-				MiniFlagController miniflagController = (MiniFlagController) miniflagLoader.getController();
-				miniflagController.setMainApp(mainApp);
-				Flag flag = GetFlag.FlagColour();
-												
-				miniflagController.instantiate(flag);
-				forecasts.add(miniflag);
-			}catch (FlagNotFoundException f){
-				System.out.println("Flag not found");
-				f.printStackTrace();
-			}
-			
-			//Current weather
-			//"Today"
-			Label todayLabel = new Label("Today");
-			todayLabel.setAlignment(Pos.CENTER);
-			todayLabel.setFont(new Font("Arial", 24.0));
-			forecasts.add(todayLabel);
-			
-			//Blade with current weather
-			WeatherObject currentWeather = mainApp.getCurrentWeatherData();
-			Node thisBlade = (Node) bladeLoader.load();
-			BladeController todayController = (BladeController) bladeLoader.getController();
-			todayController.setMainApp(mainApp);
-			DateFormat dateFormat = new SimpleDateFormat("HH:mm");
-			Date timeNow = new Date();
-			String now = dateFormat.format(timeNow);
-			todayController.instantiate(currentWeather.getTemp(),now,currentWeather.getIconURL(),currentWeather.getWindDegree(),currentWeather.getWindSpeed(),currentWeather.getDate());
-			
-			forecasts.add(thisBlade);
-			
-			//Allows us to read in another instance
-			bladeLoader.setRoot(null);
-			bladeLoader.setController(null);
-			
-			//By 3 hours data
-			boolean notFirst = false;
-			
-			//test value
-			//int total = 0;
-			for(List<WeatherObject> l : mainApp.getBy3HoursData()){
-				//Adds the correct week Day label
-				if (notFirst){
-					Label label = new Label(l.get(0).getDate().split("\\s")[0]);
-					label.setAlignment(Pos.CENTER);
-					label.setFont(new Font("Arial", 24.0));
-					forecasts.add(label);
-				}
-				//Ensures no weekday label for today
-				notFirst = true;
-
-				for(WeatherObject w: l){
-					//--Less efficient version of the hack around below--
-					//FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("Blade.fxml"));
-					//loader.setController(new BladeController(w.getTemp(),w.getTime(),w.getIconURL(),w.getWindDegree(),w.getWindSpeed(),w.getDate()));
-					//Node thisBlade = (Node) loader.load();
-					
-					
-					thisBlade = (Node) bladeLoader.load();
-					todayController = (BladeController) bladeLoader.getController();
-					todayController.setMainApp(mainApp);
-					todayController.instantiate(w.getTemp(),w.getTime(),w.getIconURL(),w.getWindDegree(),w.getWindSpeed(),w.getDate());
-					
-					forecasts.add(thisBlade);
-					
-					//Allows us to read in another instance
-					bladeLoader.setRoot(null);
-					bladeLoader.setController(null);
-					//total++;
-				}
-			}
-			
-			//test
-			//System.out.println("No Blades:" + total);
-			
-			weatherForecasts.getChildren().addAll(forecasts);
-			
-			//test
-			//System.out.println("No children of VBox: "+weatherForecasts.getChildren().size());
-			//System.out.println();
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-			
-		
+		//Populate VBOX
+		weatherForecasts.getChildren().addAll(mainApp.get3HourlyForecasts());
 		
 	}
 
--- src/SplashScreenApp.java
+++ src/SplashScreenApp.java
@@ -1,14 +1,23 @@
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class SplashScreenApp extends Application {
@@ -16,6 +25,8 @@ public class SplashScreenApp extends Application {
 		 private Stage primaryStage;
 		 private BorderPane rootLayout;
 		 private boolean by3Hours;
+		 private ObservableList<Node> dayForecasts;
+		 private ObservableList<Node> hourlyForecasts;
 		 
 		 private List<List<WeatherObject>> by3HoursData;
 		 private List<WeatherObject> byDayData;
@@ -36,6 +47,8 @@ public class SplashScreenApp extends Application {
 		     by3HoursData = WeatherDataReader.getNextFiveDaysHourly();
 		     byDayData = WeatherDataReader.getDayForecasts();
 		     currentWeatherData = WeatherDataReader.getDataForNow();
+		     calculatebyDayBlades();
+		     calculateby3HoursBlades();
 		}
 		
 		private void initRootLayout(){
@@ -75,7 +88,7 @@ public class SplashScreenApp extends Application {
 			System.out.println(Boolean.toString(by3Hours));
 		
 			FXMLLoader loader = new FXMLLoader();
-		    loader.setLocation(MainApp.class.getResource("BasicFrame.fxml"));
+		    loader.setLocation(SplashScreenApp.class.getResource("BasicFrame.fxml"));
 		    AnchorPane basicView = (AnchorPane) loader.load();
 		
 		    // Set basic view into the center of root layout.
@@ -91,7 +104,7 @@ public class SplashScreenApp extends Application {
 			by3Hours = false;
 			System.out.println(Boolean.toString(by3Hours));
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(MainApp.class.getResource("ByDay.fxml"));
+			loader.setLocation(SplashScreenApp.class.getResource("ByDay.fxml"));
 			AnchorPane basicView = (AnchorPane) loader.load();
 		
 			// Set basic view into the center of root layout.
@@ -99,11 +112,11 @@ public class SplashScreenApp extends Application {
 		   
 		   
 			//Give controller access to the main app
-		 		ByDayController controller = (ByDayController) loader.getController();
-		 		controller.setMainApp(this);
-		
-		 		return basicView;
-		    }
+	 		ByDayController controller = (ByDayController) loader.getController();
+	 		controller.setMainApp(this);
+	
+	 		return basicView;
+		}
 		 
 		    /**
 		 * Returns the main stage.
@@ -111,6 +124,133 @@ public class SplashScreenApp extends Application {
 		 */
 		public Stage getPrimaryStage() {
 		    return primaryStage;
+		}
+		
+		public void calculateby3HoursBlades(){
+			try {
+				
+				//Calculate values to populate VBox
+				hourlyForecasts = FXCollections.observableArrayList();
+				FXMLLoader bladeLoader = new FXMLLoader(SplashScreenApp.class.getResource("Blade.fxml"));
+				FXMLLoader miniflagLoader = new FXMLLoader(SplashScreenApp.class.getResource("MiniFlag.fxml"));
+				
+				//Add miniflag to top
+				try{
+					Node miniflag = (Node) miniflagLoader.load();
+					MiniFlagController miniflagController = (MiniFlagController) miniflagLoader.getController();
+					miniflagController.setMainApp(this);
+					Flag flag = GetFlag.FlagColour();
+													
+					miniflagController.instantiate(flag);
+					hourlyForecasts.add(miniflag);
+				}catch (FlagNotFoundException f){
+					System.out.println("Flag not found");
+					f.printStackTrace();
+				}
+				
+				//Current weather
+				//"Today"
+				Label todayLabel = new Label("Today");
+				todayLabel.setAlignment(Pos.CENTER);
+				todayLabel.setFont(new Font("Arial", 24.0));
+				hourlyForecasts.add(todayLabel);
+				
+				//Blade with current weather
+				WeatherObject currentWeather = this.getCurrentWeatherData();
+				Node thisBlade = (Node) bladeLoader.load();
+				BladeController todayController = (BladeController) bladeLoader.getController();
+				todayController.setMainApp(this);
+				DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+				Date timeNow = new Date();
+				String now = dateFormat.format(timeNow);
+				todayController.instantiate(currentWeather.getTemp(),now,currentWeather.getIconURL(),currentWeather.getWindDegree(),currentWeather.getWindSpeed(),currentWeather.getDate());
+				
+				hourlyForecasts.add(thisBlade);
+				
+				//Allows us to read in another instance
+				bladeLoader.setRoot(null);
+				bladeLoader.setController(null);
+				
+				//By 3 hours data
+				boolean notFirst = false;
+				
+				//test value
+				//int total = 0;
+				for(List<WeatherObject> l : this.getBy3HoursData()){
+					//Adds the correct week Day label
+					if (notFirst){
+						Label label = new Label(l.get(0).getDate().split("\\s")[0]);
+						label.setAlignment(Pos.CENTER);
+						label.setFont(new Font("Arial", 24.0));
+						hourlyForecasts.add(label);
+					}
+					//Ensures no weekday label for today
+					notFirst = true;
+
+					for(WeatherObject w: l){
+						//--Less efficient version of the hack around below--
+						//FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("Blade.fxml"));
+						//loader.setController(new BladeController(w.getTemp(),w.getTime(),w.getIconURL(),w.getWindDegree(),w.getWindSpeed(),w.getDate()));
+						//Node thisBlade = (Node) loader.load();
+						
+						
+						thisBlade = (Node) bladeLoader.load();
+						todayController = (BladeController) bladeLoader.getController();
+						todayController.setMainApp(this);
+						todayController.instantiate(w.getTemp(),w.getTime(),w.getIconURL(),w.getWindDegree(),w.getWindSpeed(),w.getDate());
+						
+						hourlyForecasts.add(thisBlade);
+						
+						//Allows us to read in another instance
+						bladeLoader.setRoot(null);
+						bladeLoader.setController(null);
+						//total++;
+					}
+				}
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		public ObservableList<Node> get3HourlyForecasts(){
+			return hourlyForecasts;
+		}
+		public void calculatebyDayBlades(){
+			try {
+				//Calculate values for by day VBox
+				dayForecasts = FXCollections.observableArrayList();
+				FXMLLoader loader = new FXMLLoader(SplashScreenApp.class.getResource("Blade.fxml"));
+				
+				//By day data
+				int day = 0;
+				boolean clickable=true;
+				for(WeatherObject w: this.getByDayData()){
+					
+					if(day>4){
+						clickable = false;
+					}
+					
+					Node thisBlade = (Node) loader.load();
+					BladeController cont = (BladeController) loader.getController();
+					cont.setMainApp(this);
+					cont.instantiate(w.getTemp(),w.getTime(),w.getIconURL(),w.getWindDegree(),w.getWindSpeed(),w.getDate(),clickable);
+					
+					dayForecasts.add(thisBlade);
+					
+					//Allows us to read in another instance
+					loader.setRoot(null);
+					loader.setController(null);
+					day++;
+				}
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		public ObservableList<Node> getByDayForecasts(){
+			return dayForecasts;
 		}
 		
 		public static void main(String[] args) {
